9923ee1b2001a3fb4d7d4789ebb1ec220a878636
Siddhant Borkar <sborkar@unc.edu>
2024-06-04 01:09:18 -0400

    Interesting stuff. Doable for now. Need to update.

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ menu.addEventListener('click', function (){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+document.querySelectorAll('.navbar__menu a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
